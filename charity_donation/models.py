68c1032e8351b0909f2783be02fa4fc81d560b88
Dablio <dawidwrzesinski96@gmail.com>
2023-01-22 15:15:00 +0100

    registration form

--- charity_donation/models.py
+++ charity_donation/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+
+class Institution(models.Model):
+
+    FUNDATIONS = (
+        (1, 'Fundacja'),
+        (2, 'Organizacja pozarządowa'),
+        (3, 'zbiórka lokalna'),
+    )
+
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    type = models.SmallIntegerField(choices=FUNDATIONS)
+    categories = models.ManyToManyField(Category)
+
+
+class Donation(models.Model):
+    quantity = models.IntegerField(default=0)
+    categories = models.ManyToManyField(Category)
+    institution = models.ForeignKey(Institution, on_delete=models.CASCADE)
+    address = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    zip_code = models.CharField(max_length=255)
+    pick_up_date = models.DateField()
+    pick_up_time = models.TimeField()
+    pick_up_comment = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=None, null=True)
--- charity_donation/views.py
+++ charity_donation/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render, redirect
 from django.views import View
 from django.http import HttpResponse
+
+from django.contrib.auth.models import User
+
+from charity_donation.models import Category, Institution, Donation
 # Create your views here.
 class LandingPage(View):
     def get(self, request):
-        return render(request, "index.html")
+        type1 = Institution.objects.filter(type=1)
+        type2 = Institution.objects.filter(type=2)
+        type3 = Institution.objects.filter(type=3)
+        num_institutions = len(list(Institution.objects.all()))
+        num_donations = len(list(Donation.objects.all()))
+        ctx = {"num_institutions": num_institutions,
+               "num_donations": num_donations,
+               "type1": type1,
+               "type2": type2,
+               "type3": type3}
+        return render(request, "index.html", ctx)
 
 
 class AddDonation(View):
@@ -20,3 +34,20 @@ class Login(View):
 class Register(View):
     def get(self, request):
         return render(request, "register.html")
+
+    def post(self, request):
+
+        name = request.POST.get("name")
+        surname = request.POST.get("surname")
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        password2 = request.POST.get("password2")
+
+        if password == password2:
+            user = User.objects.create_user(username=email, email=email, password=password, first_name=name, last_name=surname)
+        else:
+            return render(request, 'register.html', {'error': 'Passwords do not match'})
+
+        return redirect('login')
+
+
